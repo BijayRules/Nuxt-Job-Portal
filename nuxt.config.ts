01b832d694c9f2f1efe8486e5525eb62ef28ca2e
bijayshrestha14 <bijayshrestha01238@gmail.com>
2024-09-10 22:57:46 +0545

    pushg

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss',
+  modules: [
+    '@nuxtjs/tailwindcss',
     '@pinia/nuxt',
+    'nuxt-tiptap-editor',
   ],
+  tiptap: {
+    prefix: 'Tiptap', //prefix for Tiptap imports, composables not included
+  },
+
+  devtools: {
+    enabled: true,
+  },
 })
---
+++ pages/add-blog/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1 class="th-heading">Add Blog</h1><span class="th-subheading"></span>
+    <div class="th-card mt-5">
+      <form>
+        <div class="grid grid-cols-2 gap-5 ">
+          <div class="th-form"><label for="Title">Title<sup>*</sup></label><input type="text" name="Title"
+              autocomplete="off" id="Title" placeholder="Title" required="" value=""></div>
+         
+          <div class="th-form col-span-2"><label for="Description">Description<sup>*</sup></label>
+            <QuillEditor /> 
+           
+          </div>
+          <div class="th-form col-span-2 flex gap-10">
+            <div class="th-checkbox"><input type="checkbox" name="IsActive" id="IsActive" class="!w-auto"
+                checked=""><label class="!m-0" for="IsActive">Active</label></div>
+          </div>
+        </div><button type="submit" class="th-btn  mt-5"><span>Submit</span></button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'dashboard'
+})
+</script>
+
+
+<style></style>
